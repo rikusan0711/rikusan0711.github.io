a17a22c46e79cb7e0641f6903a64e18a2719bc34
rikusan0711 <rikusan0711@gmail.com>
2019-07-11 14:41:04 +0900

    TestHTML

---
+++ js/HTMLTest.js
@@ -0,0 +1,14 @@
+$(".chanegcolor1").click(function colorchange1(){
+    $('.area').css("background-image","linear-gradient(45deg,rgba(0,201,255,0.9),rgba(146,256,157,0.8)),url(./imgs/umiback.jpg)");
+    $('.circles li').css("background-color","#b2fefa");
+});
+
+$(".chanegcolor2").click(function colorchange2(){
+    $('.area').css("background-image","linear-gradient(45deg,rgba(255,106,0,0.9),rgba(238,9,121,0.8)),url(./imgs/umiback.jpg)");
+    $('.circles li').css("background-color","#ff7f50");
+});
+
+$(".chanegcolor3").click(function colorchange3(){
+    $('.area').css("background-image","linear-gradient(45deg,rgba(54,0,51,0.9),rgba(11,135,147,0.8)),url(./imgs/umiback.jpg)");
+    $('.circles li').css("background-color","#483d8b");
+});
